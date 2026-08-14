d3ec05a1308c04211e007c13b527d3ee9375127f
Sadik Khan <khansadik2077@gmail.com>
2026-08-14 15:09:57 +0600

    DBconnection Changed

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,9 +1,9 @@
 import java.sql.*;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/Academia";
-    private static final String USER = "sadik";
-    private static final String PASS = "Mysql@123";
+    private static final String URL = "jdbc:mysql://localhost:3306/your_database";
+    private static final String USER = "user";
+    private static final String PASS = "Your_pass";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
